da194e67f60323f0278230a5b9305587aa74a334
nattawat thongvichian <the_kingston2012@hotmail.com>
2019-07-13 14:13:53 +0700

    เิ่มหน้าเทส

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const app = express();
 const port = 3000
 
 app.get('/', (req, res) => res.send('Nattawat'))
-app.get('/about', (req, res) => res.send('เกี่ยวกับ'))
+app.get('/about', (req, res) => res.send('เกี่ยวกับg'))
 app.get('/contact', (req, res) => res.send('หน้าติดต่อเรา'))
+app.get('/test', (req, res) => {
+    res.json({message: "test"})
+ 
+})
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
