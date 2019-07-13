e0357865880a17388025c7f57933b8c58bef292a
nattawat thongvichian <the_kingston2012@hotmail.com>
2019-07-13 13:58:28 +0700

    Hello github

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express();
+
+const port = 3000
+
+app.get('/', (req, res) => res.send('Nattawat'))
+app.get('/about', (req, res) => res.send('เกี่ยวกับ'))
+app.get('/contact', (req, res) => res.send('หน้าติดต่อเรา'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
